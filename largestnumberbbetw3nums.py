0b518b34606c38b8585f530fbc34ce3090ab6594
Gaurav Hungund <60032249+AsgardRodent@users.noreply.github.com>
2020-08-07 15:30:49 +0530

    Add files via upload

---
+++ largestnumberbbetw3nums.py
@@ -0,0 +1,10 @@
+a =int(input("Enter first number"))
+b =int(input("Enter second number"))
+c =int(input("Enter third number"))
+
+if a >=c and a >=b:
+    print(a," is the greatest number  ")
+elif b >= a and b >= c:
+    print(b," is the greatest number  ")
+else:
+    print(c," is the greatest number  ")
