5088b077c152ff2a82634bbbc41fda945f6ddf36
Gaurav Hungund <60032249+AsgardRodent@users.noreply.github.com>
2020-08-18 02:20:23 +0530

    Add files via upload

---
+++ return.py
@@ -0,0 +1,12 @@
+#return functions
+# return is a keyword that return the value just like return 0 we type in a cpp program
+
+
+def add(num1,num2):
+    #they are default parameters
+    return num1 + num2
+
+sum1 = add(10,20)
+print(sum1)
+sum2 = add(30,40)
+print(sum2)
