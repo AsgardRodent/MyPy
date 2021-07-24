3ad556674a00b94fbf8b418364927fb974f5a794
Gaurav Hungund <60032249+AsgardRodent@users.noreply.github.com>
2021-07-24 17:30:19 +0530

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,42 +1,5 @@
-#calc
-c = int
-def addition(x,y):
-    c  = x+y
-    return c
+#largest number
 
-def sub(x,y):
-    c = x-y
-    return c
-
-def mult(x,y):
-    c =x * y
-    return c
-
-def divide(x,y):
-    c = x/y
-    return c
-
-print("welcome to my python calc")
-print("what operation would you like to perform on two numbers ?")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-while True:
-    choice = input("Enter choice here :: ")
-    if choice in ('1','2','3','4'):
-        x = int(input("Enter first number"))
-        y = int(input("Enter second number"))
-
-        if choice  == '1':
-            print(x,"+",y,"=",addition(x,y))
-        elif choice == '2':
-            print(x,"-",y,"=",sub(x,y))
-        elif choice == '3':
-            print(x,"*",y,"=",mult(x,y))
-        elif choice == '4':
-            print(x,"/",y,"=",divide(x,y))
-        break
-    else:
-        print("invalid choice")
+a =int(input("Enter first number"))
+b =int(input("Enter second number"))
+c =int(input("Enter third number"))
